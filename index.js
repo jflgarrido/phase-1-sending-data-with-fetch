12f0ab729221230f91a8467c437472b76092206c
Julius Garrido <jflgarrido@gmail.com>
2021-11-22 09:11:31 -0700

    commit tree 16c155910a2b044d14d902ac2d280ec3f4787996

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+
+const configureObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    body: JSON.stringify(formData),
+    };
+
+
+function submitData() {
+    return fetch('http://localhost:3000/users', configureObject)
+    .then(res => res.json())
+    .then(data => document.body.innerHTML = data.id)
+    .catch(function (error) {
+        document.body.innerHTML = error.message;
+        console.log(error.message);
+      });
+    } 
+
+submitData();
